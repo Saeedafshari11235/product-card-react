4850d53a4794144fb270c15d2bc6232afa577eb3
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-25 16:37:13 +0330

    add props to the component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Card from './components/card';
 
 function App() {
   return (
     <div className="App">
+      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
     </div>
   );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -2,21 +2,21 @@ import React from "react";
 import productPic from "./product_pic.webp";
 import "./card.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <div className="image-container">
         <img src={productPic} alt="product" />
       </div>
       <div className="title-container">
-        <h4>headphone headphone headphone</h4>
+        <h4>{props.title}</h4>
       </div>
       <div className="num-container">
         <div className="score-container">
-          <span>5⭐</span>
+          <span>{props.score}⭐</span>
         </div>
         <div className="price-container">
-          <span>{10_000_000} T</span>
+          <span>{props.price} T</span>
         </div>
       </div>
       <div className="buttons-container">
