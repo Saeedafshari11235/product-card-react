2a88c6453a81fcabe3b13db2dcd062a33236fc0e
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-25 20:02:31 +0330

    Complete the card containers style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
+      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
+      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
+      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
+      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
+      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
+      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
     </div>
   );
 }
