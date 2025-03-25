c11e9882b788efe7334e8f4d78fe720f4b1b0736
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-26 02:14:09 +0330

    add spread syntax to props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import Card from './components/card';
 
+let products = [
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
+]
+
 function App() {
   return (
     <div className="App">
-      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
-      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
-      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
-      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
-      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
-      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
-      <Card title="Onikoma X20 (3.5) gaming headset" score="5" price={10_000_000} ></Card>
+      <Card {...products[0]} ></Card>
+      <Card {...products[1]} ></Card>
+      <Card {...products[2]} ></Card>
+      <Card {...products[3]} ></Card>
+      <Card {...products[4]} ></Card>
+      <Card {...products[5]} ></Card>
+      <Card {...products[6]} ></Card>
     </div>
   );
 }
