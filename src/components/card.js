68eb4049fa2f104a9634d26ba3d06d5f9fa18f18
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-25 16:27:33 +0330

    Add numbers container style

--- src/components/card.js
+++ src/components/card.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import productPic from './product_pic.webp'
-import './card.css'
+import React from "react";
+import productPic from "./product_pic.webp";
+import "./card.css";
 
 export default function Card() {
   return (
-    <div className='card'>
-        <div className='image-container'>
-            <img src={productPic} alt='product'/>
+    <div className="card">
+      <div className="image-container">
+        <img src={productPic} alt="product" />
+      </div>
+      <div className="title-container">
+        <h4>headphone headphone headphone</h4>
+      </div>
+      <div className="num-container">
+        <div className="score-container">
+          <span>5⭐</span>
         </div>
-        <div className='title-container'>
-            <h4>headphone headphone headphone</h4>
+        <div className="price-container">
+          <span>{10_000_000} T</span>
         </div>
-        <div className='score-container'>
-            <span>5⭐</span>
-        </div>
-        <div className='price-container'>
-            <span>{10_000_000} T</span>
-        </div>
-        <div className='buttons-container'>
-            <button>button</button>
-        </div>
+      </div>
+      <div className="buttons-container">
+        <button>button</button>
+      </div>
     </div>
-  )
+  );
 }
