5d0dfb9386d1b91bde4fcecd100bb1e6a25e33c5
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-25 14:20:46 +0330

    Add component tag structure

--- src/components/card.js
+++ src/components/card.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import './card.css'
 
-export default function card() {
+export default function Card() {
   return (
-    <div>card</div>
+    <div className='card'>
+        <div className='image-container'>
+            <img src='' alt=''/>
+        </div>
+        <div className='header-container'>
+            <h4>header</h4>
+        </div>
+        <div className='category-container'>
+            <span>category</span>
+        </div>
+        <div className='description-container'>
+            <p>
+                description
+            </p>
+        </div>
+        <div className='buttons-container'>
+            <button>button</button>
+        </div>
+    </div>
   )
 }
