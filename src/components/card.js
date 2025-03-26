995b0fb022b6a4300798d857aee047071e7dcd3d
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-27 02:51:17 +0330

    Change props style to destructuring

--- src/components/card.js
+++ src/components/card.js
@@ -2,21 +2,21 @@ import React from "react";
 import productPic from "./product_pic.webp";
 import "./card.css";
 
-export default function Card(props) {
+export default function Card({title, score, price}) {
   return (
     <div className="card">
       <div className="image-container">
         <img src={productPic} alt="product" />
       </div>
       <div className="title-container">
-        <h4>{props.title}</h4>
+        <h4>{title}</h4>
       </div>
       <div className="num-container">
         <div className="score-container">
-          <span>{props.score}⭐</span>
+          <span>{score}⭐</span>
         </div>
         <div className="price-container">
-          <span>{props.price} T</span>
+          <span>{price} T</span>
         </div>
       </div>
       <div className="buttons-container">
