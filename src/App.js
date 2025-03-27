44baf8831d65c71b636598a9e76da0a4f2df5188
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-03-27 10:26:05 +0330

    update the component importing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import './App.css';
-import Card from './components/card';
+import "./App.css";
+import Card from "./components/card";
 
 let products = [
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-  {title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000},
-]
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+  { title: "Onikoma X20 (3.5) gaming headset", score: "5", price: 10_000_000 },
+];
 
 function App() {
   return (
     <div className="App">
-      <Card {...products[0]} ></Card>
-      <Card {...products[1]} ></Card>
-      <Card {...products[2]} ></Card>
-      <Card {...products[3]} ></Card>
-      <Card {...products[4]} ></Card>
-      <Card {...products[5]} ></Card>
-      <Card {...products[6]} ></Card>
+      {products.map((item) => (
+        <Card {...item}></Card>
+      ))}
     </div>
   );
 }
